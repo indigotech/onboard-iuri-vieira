032fff63887b6ad602f22d329c1c1501b9a22192
iuri-unb <iurisousavieira@gmail.com>
2021-10-14 10:25:31 -0300

    index now initiate the server and connection to db

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import { User } from "./entity/User";
+import * as express from "express";
+import { ApolloServer } from "apollo-server-express";
 
-createConnection()
-  .then(async (connection) => {
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Iuri";
-    user.lastName = "Sousa";
-    user.age = 22;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+import typeDefs from "./typeDefs";
+import resolvers from "./resolvers";
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-  })
-  .catch((error) => console.log(error));
+const startServer = async () => {
+  try {
+    const server = new ApolloServer({ typeDefs, resolvers });
+
+    await createConnection();
+
+    const app = express();
+
+    await server.start();
+
+    server.applyMiddleware({ app });
+
+    app.listen({ port: 4000 }, () =>
+      console.log(
+        `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+      )
+    );
+  } catch (error) {
+    console.log(error);
+    console.log("An error occured, try again later");
+  }
+};
+
+startServer();
