201820cab1522661e3e49bb56e9ac65200bace86
iuri-unb <iurisousavieira@gmail.com>
2021-10-14 10:23:01 -0300

    changed schema to a single file

---
+++ src/typeDefs.ts
@@ -0,0 +1,26 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+
+  type Mutation {
+    createUser(
+      name: String!
+      email: String!
+      password: String!
+      birthDate: String!
+    ): UserInput
+  }
+
+  type UserInput {
+    id: String!
+    name: String!
+    email: String!
+    password: String!
+    birthDate: String!
+  }
+`;
+
+export default typeDefs;
