74707360d9d513be52acc2adb2ddf3f13a06c7d6
iuri-unb <iurisousavieira@gmail.com>
2021-10-15 14:56:42 -0300

    added hello query test

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,5 +1,29 @@
+import startServer from "../index";
+
+const url = `http://localhost:4000/graphql`;
+const request = require("supertest")(url);
+
+before(async () => {
+  await startServer();
+});
+
 describe("First test", function () {
   it("should print something on terminal", function () {
     console.log("The tests are working!");
+  });
+});
+
+describe("Hello query", function () {
+  it("shoud return a Hello word string", (done) => {
+    request
+      .post("/")
+      .send({ query: "query { hello }" })
+      .set("Accept", "application/json")
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        console.log(res.body.data.hello);
+        done();
+      });
   });
 });
