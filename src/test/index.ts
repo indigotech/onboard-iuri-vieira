2a58a5e6cf33bb21b2fc0d3cdd401076559c75ec
iuri-unb <iurisousavieira@gmail.com>
2021-10-22 08:14:54 -0300

    resolved the conflicts from rebase

--- src/test/index.ts
+++ src/test/index.ts
@@ -3,32 +3,17 @@ import { expect } from "chai";
 import { getRepository } from "typeorm";
 import { User } from "../entity/User";
 import * as dotenv from "dotenv";
-import * as request from "supertest";
-import { queryRequest } from "./request-functions";
+import { queryRequest, mutationRequest } from "./request-functions";
 import { UserInput } from "../typeDefs";
 
-const createUserMutation = `mutation {
-  createUser(data: {name: "Name Test", email: "test@mail.com", password: "123456teste", birthDate: "06-05-1999"}){
+const createUserMutation = `mutation CreateUserMutation($data: UserInput!) {
+  createUser(data: $data) {
     id
     name
     email
     birthDate
   }
 }`;
-
-const data: UserInput = {
-  name: "Name Test",
-  email: "test@mail.com",
-  password: "123456teste",
-  birthDate: "06-05-1999",
-};
-
-const dataPasswordError: UserInput = {
-  name: "Name Test",
-  email: "test@mail.com",
-  password: "12345c",
-  birthDate: "06-05-1999",
-};
 
 before(async () => {
   dotenv.config({ path: `${__dirname}/../../test.env` });
@@ -37,16 +22,20 @@ before(async () => {
 
 describe("Hello query", function () {
   it("shoud return a Hello word string", async () => {
-    const response = await queryRequest(
-      `http://localhost:${process.env.PORT}/graphql`,
-      "query { hello }"
-    );
+    const response = await queryRequest("query { hello }");
 
     expect(response.body.data.hello).to.equal("Hello world!");
   });
 });
 
 describe("createUser mutation", function () {
+  let data: UserInput = {
+    name: "Name Test",
+    email: "test@mail.com",
+    password: "123456teste",
+    birthDate: "06-05-1999",
+  };
+
   afterEach(async () => {
     const db = getRepository(User);
     await db.clear();
@@ -54,71 +43,8 @@ describe("createUser mutation", function () {
     expect(clear).to.equal(0);
   });
 
-  it("should return that the email is already in use error", async () => {
-    const responseCreateUser = await request(
-      `http://localhost:${process.env.PORT}/graphql`
-    )
-      .post("/")
-      .set("Accept", "application/json")
-      .send({
-        query: createUserMutation,
-        variables: { data },
-      })
-      .expect(200);
-
-    expect(responseCreateUser.body.data.createUser.id).to.greaterThan(0);
-    expect(responseCreateUser.body.data.createUser.name).to.equal("Name Test");
-    expect(responseCreateUser.body.data.createUser.email).to.equal(
-      "test@mail.com"
-    );
-    expect(responseCreateUser.body.data.createUser.birthDate).to.equal(
-      "06-05-1999"
-    );
-
-    const response = await request(
-      `http://localhost:${process.env.PORT}/graphql`
-    )
-      .post("/")
-      .set("Accept", "application/json")
-      .send({
-        query: createUserMutation,
-        variables: { data },
-      })
-      .expect(200);
-
-    expect(response.body.errors[0].code).to.equal(409);
-    expect(response.body.errors[0].message).to.equal(
-      "This email is already in use!"
-    );
-  });
-
-  it("should return an invalid password error", async () => {
-    const response = await request(
-      `http://localhost:${process.env.PORT}/graphql`
-    )
-      .post("/")
-      .set("Accept", "application/json")
-      .send({
-        query: createUserMutation,
-        variables: {
-          data: {
-            name: "Name Test",
-            email: "test@mail.com",
-            password: "12345c",
-            birthDate: "06-05-1999",
-          },
-        },
-      });
-
-    expect(response.body.errors[0].code).to.equal(400);
-    expect(response.body.errors[0].message).to.equal("Invalid password!");
-  });
-
   it("should insert a user", async () => {
-    const response = await queryRequest(
-      `http://localhost:${process.env.PORT}/graphql`,
-      createUserMutation
-    );
+    const response = await mutationRequest(createUserMutation, { data });
 
     const id = response.body.data.createUser.id;
     const user = await getRepository(User).findOne({ id });
@@ -133,6 +59,36 @@ describe("createUser mutation", function () {
       email: user.email,
       birthDate: user.birthDate,
     });
+  });
+
+  it("should return that the email is already in use error", async () => {
+    await getRepository(User).insert({
+      name: "Name Test",
+      email: "test@mail.com",
+      password: "123456teste",
+      birthDate: "06-05-1999",
+    });
+
+    const response = await mutationRequest(createUserMutation, { data });
+
+    expect(response.body.errors[0].code).to.equal(409);
+    expect(response.body.errors[0].message).to.equal(
+      "This email is already in use!"
+    );
+  });
+
+  it("should return an invalid password error", async () => {
+    data = {
+      name: "Name Test",
+      email: "test@mail.com",
+      password: "12345c",
+      birthDate: "06-05-1999",
+    };
+
+    const response = await mutationRequest(createUserMutation, { data });
+
+    expect(response.body.errors[0].code).to.equal(400);
+    expect(response.body.errors[0].message).to.equal("Invalid password!");
   });
 });
 
@@ -157,7 +113,6 @@ describe("getUser query", function () {
     const userId = user.id;
 
     const response = await queryRequest(
-      `http://localhost:${process.env.PORT}/graphql`,
       `query {
         getUser(id: ${userId}) {
           id
