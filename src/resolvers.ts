b96c081300dff6202053427e0a6c9bfda9b95605
iuri-unb <iurisousavieira@gmail.com>
2021-10-22 10:40:15 -0300

    returning the token on login mutation

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,7 +1,8 @@
 import { User } from "./entity/User";
 import { getConnection, getRepository } from "typeorm";
 import { CustomError } from "./error";
-const bcrypt = require("bcrypt");
+import * as jwt from "jsonwebtoken";
+import * as bcrypt from "bcrypt";
 
 const resolvers = {
   Query: {
@@ -84,7 +85,10 @@ const resolvers = {
       const validPassword = await bcrypt.compare(password, user.password);
 
       if (validPassword) {
-        const response = { user: user, token: "the_token" };
+        const token = jwt.sign({ username: user.email }, "supersecret", {
+          expiresIn: 3600,
+        });
+        const response = { user: user, token: token };
         return response;
       } else {
         throw new CustomError(
