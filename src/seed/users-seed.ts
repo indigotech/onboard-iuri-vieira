e1fb5ca68335bcab9f2bf137d7c0173ce53c71db
iuri-unb <iurisousavieira@gmail.com>
2021-10-25 17:26:44 -0300

    rmved startServer because its using getRepository

--- src/seed/users-seed.ts
+++ src/seed/users-seed.ts
@@ -2,13 +2,8 @@ import { getRepository } from "typeorm";
 import { User } from "../entity/User";
 import * as faker from "faker";
 import * as bcrypt from "bcrypt";
-import startServer from "../connect";
-import * as dotenv from "dotenv";
 
 export const seedUser = async () => {
-  dotenv.config({ path: `${__dirname}/../../test.env` });
-  await startServer();
-
   const userRepository = await getRepository(User);
   const salt = await bcrypt.genSalt(10);
 
