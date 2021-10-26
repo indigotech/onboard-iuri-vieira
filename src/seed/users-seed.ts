3404c07d2619ead0bdc9c4e2e530bc6afa7ac848
iuri-unb <iurisousavieira@gmail.com>
2021-10-26 08:44:46 -0300

    seed to insert users in user table

--- src/seed/users-seed.ts
+++ src/seed/users-seed.ts
@@ -2,21 +2,34 @@ import { getRepository } from "typeorm";
 import { User } from "../entity/User";
 import * as faker from "faker";
 import * as bcrypt from "bcrypt";
+import { CustomError } from "../error";
+import * as dotenv from "dotenv";
+import startServer from "../connect";
 
 export const seedUser = async () => {
-  const userRepository = await getRepository(User);
-  const salt = await bcrypt.genSalt(10);
+  try {
+    dotenv.config();
+    await startServer();
 
-  for (let index = 0; index < 50; index++) {
-    const user = new User();
-    user.name = faker.name.firstName();
-    user.birthDate = faker.date.past().toDateString();
-    user.email = faker.internet.email(user.name);
-    user.password = user.password = await bcrypt.hash(
-      faker.random.alphaNumeric(10),
-      salt
-    );
+    const userRepository = getRepository(User);
+    const salt = await bcrypt.genSalt(10);
 
-    await userRepository.save(user);
+    for (let index = 0; index < 50; index++) {
+      const user = new User();
+      user.name = faker.name.firstName();
+      user.birthDate = faker.date.past().toDateString();
+      user.email = faker.internet.email(user.name);
+      user.password = user.password = await bcrypt.hash(
+        faker.random.alphaNumeric(10),
+        salt
+      );
+
+      await userRepository.save(user);
+    }
+  } catch (error) {
+    console.log(error);
+    throw new CustomError(400, "Error trying to execute function");
   }
 };
+
+seedUser();
