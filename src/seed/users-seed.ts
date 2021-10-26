02555794f8eeb407987ae76ce85798d4a246b220
iuri-unb <iurisousavieira@gmail.com>
2021-10-26 16:54:42 -0300

    cahnged the array name to useres

--- src/seed/users-seed.ts
+++ src/seed/users-seed.ts
@@ -13,18 +13,18 @@ export const seedUser = async () => {
 
     const userRepository = getRepository(User);
     const salt = await bcrypt.genSalt(10);
-    let user = [];
+    let users = [];
     for (let index = 0; index < 50; index++) {
-      user[index] = new User();
-      user[index].name = faker.name.firstName();
-      user[index].birthDate = faker.date.past().toDateString();
-      user[index].email = faker.internet.email(user[index].name);
-      user[index].password = await bcrypt.hash(
+      users[index] = new User();
+      users[index].name = faker.name.firstName();
+      users[index].birthDate = faker.date.past().toDateString();
+      users[index].email = faker.internet.email(users[index].name);
+      users[index].password = await bcrypt.hash(
         faker.random.alphaNumeric(10),
         salt
       );
     }
-    await userRepository.save(user);
+    await userRepository.save(users);
   } catch (error) {
     console.log(error);
     throw new CustomError(400, "Error trying to execute function");
